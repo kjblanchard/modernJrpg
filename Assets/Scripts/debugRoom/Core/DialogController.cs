802c7648070f28747c23be5c0dad630e9a0af43c
Kevin Blanchard <adkb-at-145035066069>
2021-09-25 16:16:25 -0400

    Finish the typewriter text

--- Assets/Scripts/debugRoom/Core/DialogController.cs
+++ Assets/Scripts/debugRoom/Core/DialogController.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 /// <summary>
 /// The Controller that handles updating the gui from player events.
@@ -13,24 +15,27 @@ public class DialogController : MonoBehaviour
     public bool InDialog { get; private set; }
 
     [SerializeField] private TMP_Text _textBoxToUpdate;
-    [SerializeField] private DotweenBroadcasterComponent dialogBoxOpenTween;
-    [SerializeField] private DotweenBroadcasterComponent dialogBoxCloseTween;
+    [SerializeField] private DotweenBroadcasterComponent _dialogBoxOpenTween;
+    [SerializeField] private DotweenBroadcasterComponent _dialogBoxCloseTween;
+    [SerializeField] private float _timeBetweenTypewriterTyping = 0.1f;
 
     private bool _dialogBoxLoading;
     private int _currentLocationInDialog;
     private Dialog _dialogToDisplay;
-    private const string dialogBoxOpen = "dialogBoxOpen";
-    private const string dialogBoxClosed = "dialogBoxClose";
-
+    private const string _dialogBoxOpen = "dialogBoxOpen";
+    private const string _dialogBoxClosed = "dialogBoxClose";
     private bool _typewriterTyping;
+    private Coroutine _currentTypewriterCoroutine;
+    private WaitForSeconds _timeBetweenTypewriterTypingWait;
 
     /// <summary>
     /// Register to watch for dialogBox animation completion events. On startup
     /// </summary>
     private void Start()
     {
-        dialogBoxOpenTween.DotweenCompleteEvent += OnDialogBoxOpenCompletedEvent;
-        dialogBoxCloseTween.DotweenCompleteEvent += OnDialogBoxClosedCompletedEvent;
+        _dialogBoxOpenTween.DotweenCompleteEvent += OnDialogBoxOpenCompletedEvent;
+        _dialogBoxCloseTween.DotweenCompleteEvent += OnDialogBoxClosedCompletedEvent;
+        _timeBetweenTypewriterTypingWait = new WaitForSeconds(_timeBetweenTypewriterTyping);
     }
 
 
@@ -43,11 +48,16 @@ public class DialogController : MonoBehaviour
     {
         if (_dialogBoxLoading)
             return;
-        if (InDialog)
+        switch (InDialog)
         {
-            AdvanceDialog();
-            return;
+            case true when _typewriterTyping:
+                StopTypewriter();
+                return;
+            case true when !_typewriterTyping:
+                AdvanceDialog();
+                return;
         }
+
         InitializeDialogInteraction(dialogToGoThrough);
     }
 
@@ -59,7 +69,7 @@ public class DialogController : MonoBehaviour
         _textBoxToUpdate.gameObject.SetActive(false);
         _textBoxToUpdate.text = dialogToGoThrough.LinesOfDialog[_currentLocationInDialog].Dialog;
         _dialogToDisplay = dialogToGoThrough;
-        DOTween.Play(dialogBoxOpen);
+        DOTween.Play(_dialogBoxOpen);
     }
 
 
@@ -67,7 +77,7 @@ public class DialogController : MonoBehaviour
     {
         if (_currentLocationInDialog + 1 >= _dialogToDisplay.LinesOfDialog.Length)
         {
-            DOTween.Play(dialogBoxClosed);
+            DOTween.Play(_dialogBoxClosed);
             return;
         }
         _currentLocationInDialog++;
@@ -79,30 +89,91 @@ public class DialogController : MonoBehaviour
     {
         InDialog = false;
         _currentLocationInDialog = 0;
-        DOTween.Rewind(dialogBoxClosed);
-        DOTween.Rewind(dialogBoxOpen);
+        DOTween.Rewind(_dialogBoxClosed);
+        DOTween.Rewind(_dialogBoxOpen);
     }
 
     private void StartDialogPlayerInteraction(Dialog dialogToGoThrough)
     {
         _dialogBoxLoading = false;
         _textBoxToUpdate.gameObject.SetActive(true);
+        InitializeTypewriterEffect();
     }
 
 
-    public void OnDialogBoxOpenCompletedEvent(object thing, EventArgs e)
+    private void OnDialogBoxOpenCompletedEvent(object thing, EventArgs e)
     {
 
         StartDialogPlayerInteraction(_dialogToDisplay);
     }
-    public void OnDialogBoxClosedCompletedEvent(object thing, EventArgs e)
+    private void OnDialogBoxClosedCompletedEvent(object thing, EventArgs e)
     {
         EndDialog();
     }
 
-    private void TypewriterEffect()
+    private void InitializeTypewriterEffect()
     {
+        _typewriterTyping = true;
+        _currentTypewriterCoroutine = StartCoroutine(RevealCharacters(_textBoxToUpdate));
+    }
+
+
+    private void StopTypewriter()
+    {
+        if (!_typewriterTyping)
+            return;
+        StopCoroutine(_currentTypewriterCoroutine);
+        _typewriterTyping = false;
+        RevealAllCharacters(_textBoxToUpdate);
+
 
     }
+    /// <summary>
+    /// Method revealing the text one character at a time.
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator RevealCharacters(TMP_Text textComponent)
+    {
+        textComponent.ForceMeshUpdate();
+        var textInfo = textComponent.textInfo;
+        var totalVisibleCharacters = textInfo.characterCount; // get # of visible character in text object
+        var charactersVisible = 0;
+        var shouldContinue = true;
+
+        while (shouldContinue)
+        {
+
+            if (charactersVisible >= totalVisibleCharacters)
+            {
+                EndTypewriterText();
+                shouldContinue = false;
+            }
+
+            textComponent.maxVisibleCharacters = charactersVisible; // How many characters should TextMeshPro display?
+
+            charactersVisible += 1;
+
+            yield return _timeBetweenTypewriterTypingWait;
+        }
+    }
+
+    /// <summary>
+    /// Modifies the values to stop the typewriter
+    /// </summary>
+    private void EndTypewriterText()
+    {
+        _currentTypewriterCoroutine = null;
+        _typewriterTyping = false;
+    }
+
+    /// <summary>
+    /// Reveals all the characters when the typewriter is typing and the player presses rightclick
+    /// </summary>
+    /// <param name="textComponent"></param>
+    private void RevealAllCharacters(TMP_Text textComponent)
+    {
+        textComponent.maxVisibleCharacters = int.MaxValue;
+    }
+
 
 }
