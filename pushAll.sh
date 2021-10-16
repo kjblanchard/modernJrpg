1164f34e46f85f17a73c4d1bb90939b8a0dbc706
Kevin Blanchard <adkb-at-145035066069>
2021-10-15 20:42:26 -0400

    Updated pushall

--- pushAll.sh
+++ pushAll.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-git add . && git commit -m "$1" && git push origin master && git push origin github &
+git add . && git commit -m "$1" && git push origin master && git push github master &
